43351a90d2753542f1c2ccf5fef5f5222ea49e52
rahulg2001 <67199830+rahulg2001@users.noreply.github.com>
2020-06-30 12:05:12 +0100

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,10 @@
+
+//Creating Hello World Example
+//Let's create the hello java program:
+
+
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}
